b825cb6bd9a9e287776b7c5df51f1ebac34b6e27
Santhosh Balusamy <santhosh.balusamy@freshworks.com>
2023-01-23 10:37:32 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,9 @@ export default () => {
                     globals: {
                         vue: 'Vue'
                     }
+                },
+                resolve: {
+                    dedupe: ['vue'],
                 }
             }
         },
